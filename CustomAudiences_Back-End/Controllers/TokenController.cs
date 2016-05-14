0c6fe89b838d820a7494e22fb17a4556dd350f75
Junior Ilheu <jilheu07@hotmail.com>
2016-05-14 02:47:23 -0400

    Added Conrollers and demo Ajax Post

---
+++ CustomAudiences_Back-End/Controllers/TokenController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Cors;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CustomAudiences_Back_End.Controllers
+{
+    [Route("api/[controller]")]
+    public class TokenController : Controller
+    {
+        // GET: api/Token/
+        [HttpGet]
+        public string  Get()
+        {
+            return "hola soy el token";
+        }
+        // POST api/Token
+        [HttpPost("")]
+        public string Post([FromBody]string accessToken)
+        {
+            //var jData = Json.d;
+            var token = accessToken;
+            return token;
+        }
+    }
+
+}
